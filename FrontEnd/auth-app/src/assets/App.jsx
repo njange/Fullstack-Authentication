5b3ed3a1f16efa76cadf6d142f340955b213e26e
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-03-23 17:55:50 +0300

    app

--- FrontEnd/auth-app/src/assets/App.jsx
+++ FrontEnd/auth-app/src/assets/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-
+import {BrowserRouter, Routers, Route} from "react-router-dom"; 
 
 function App() {
     return (
-        <></>
+        <BrowserRouter>
+        <Routes>
+            <Route path= '/' element ={<Home />}></Route>
+        </Routes>
+        </BrowserRouter>
     )
 }
 
